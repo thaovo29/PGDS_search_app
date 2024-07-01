ea893c33dbc133a1685c7a6243c636c0edcd1944
thaovo29 <thaovo2962002@gmail.com>
2024-07-01 21:49:39 +0700

    search_app

---
+++ hello_milvus.py
@@ -0,0 +1,204 @@
+# pgds.py demonstrates the basic operations of PyMilvus, a Python SDK of Milvus.
+# 1. connect to Milvus
+# 2. create collection
+# 3. insert data
+# 4. create index
+# 5. search, query, and hybrid search on entities
+# 6. delete entities by PK
+# 7. drop collection
+import time
+
+import numpy as np
+from glob import glob
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema, CollectionSchema, DataType,
+    Collection,
+)
+
+def list_file_in_folder(path, ext):
+    return glob(path + f"/*.{ext}")
+
+fmt = "\n=== {:30} ===\n"
+search_latency_fmt = "search latency = {:.4f}s"
+# num_entities, dim = 3000, 8
+dim = 768
+
+#################################################################################
+# 1. connect to Milvus
+# Add a new connection alias `default` for Milvus server in `localhost:19530`
+# Actually the "default" alias is a buildin in PyMilvus.
+# If the address of Milvus is the same as `localhost:19530`, you can omit all
+# parameters and call the method as: `connections.connect()`.
+#
+# Note: the `using` parameter of the following methods is default to "default".
+print(fmt.format("start connecting to Milvus"))
+connections.connect("default", host="localhost", port="19530")
+
+has = utility.has_collection("pgds")
+print(f"Does collection pgds exist in Milvus: {has}")
+if has:
+    print(fmt.format("Drop collection `pgds`"))
+    utility.drop_collection("pgds")
+
+#################################################################################
+# 2. create collection
+# We're going to create a collection with 3 fields.
+# +-+------------+------------+------------------+------------------------------+
+# | | field name | field type | other attributes |       field description      |
+# +-+------------+------------+------------------+------------------------------+
+# |1|    "pk"    |   VarChar  |  is_primary=True |      "primary field"         |
+# | |            |            |   auto_id=False  |                              |
+# +-+------------+------------+------------------+------------------------------+
+# |2|"embeddings"| FloatVector|     dim=768      |  "float vector with dim 768" |
+# +-+------------+------------+------------------+------------------------------+
+fields = [
+    FieldSchema(name="pk", dtype=DataType.VARCHAR, is_primary=True, auto_id=False, max_length=100),
+    FieldSchema(name="filepath", dtype=DataType.VARCHAR, description="file path", is_primary=False, max_length=1000),
+    FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=dim)
+]
+
+schema = CollectionSchema(fields, "PGDS schema")
+
+print(fmt.format("Create collection `pgds`"))
+pgds = Collection("pgds", schema, consistency_level="Strong")
+
+################################################################################
+# 3. insert data
+# We are going to insert 3000 rows of data into `pgds`
+# Data to be inserted must be organized in fields.
+#
+# The insert() method returns:
+# - either automatically generated primary keys by Milvus if auto_id=True in the schema;
+# - or the existing primary key field from the entities if auto_id=False in the schema.
+
+print(fmt.format("Start inserting entities"))
+embedding_vectors = np.load("/Users/JCPHF7G4NK/Documents/projects/thesis/PGDS/DukeMTMC-reID/embeddedVector.npy")
+n_vector = len(embedding_vectors)
+files = list_file_in_folder("/Users/JCPHF7G4NK/Documents/projects/thesis/PGDS/DukeMTMC-reID/bounding_box_test", "jpg")
+entities = []
+for i in range(n_vector):
+    entities.append({
+        # provide the pk field because `auto_id` is set to False
+        "pk": str(i),
+        # rng.random(num_entities).tolist(),  # field random, only supports list
+        "filepath" : files[i],
+        "embeddings" : embedding_vectors[i][0],    # field embeddings, supports numpy.ndarray and list
+    }
+    )
+
+insert_result = pgds.insert(entities)
+
+pgds.flush()
+print(f"Number of entities in Milvus: {pgds.num_entities}")  # check the num_entites
+
+################################################################################
+# 4. create index
+# We are going to create an IVF_FLAT index for pgds collection.
+# create_index() can only be applied to `FloatVector` and `BinaryVector` fields.
+print(fmt.format("Start Creating index IVF_FLAT"))
+index = {
+    "index_type": "IVF_FLAT",
+    "metric_type": "COSINE",
+    "params": {"nlist": 128},
+}
+
+pgds.create_index("embeddings", index)
+
+################################################################################
+# 5. search, query, and hybrid search
+# After data were inserted into Milvus and indexed, you can perform:
+# - search based on vector similarity
+# - query based on scalar filtering(boolean, int, etc.)
+# - hybrid search based on vector similarity and scalar filtering.
+#
+
+# Before conducting a search or a query, you need to load the data in `pgds` into memory.
+print(fmt.format("Start loading"))
+pgds.load()
+
+# -----------------------------------------------------------------------------
+# search based on vector similarity
+# print(fmt.format("Start searching based on vector similarity"))
+# vectors_to_search = embedding_vectors[0]
+# search_params = {
+#     "metric_type": "COSINE",
+#     "params": {},
+# }
+
+# start_time = time.time()
+# result = pgds.search(vectors_to_search, "embeddings", search_params, limit=3, output_fields=["filepath"])
+# end_time = time.time()
+
+# for hits in result:
+#     for hit in hits:
+#         print(f"hit: {hit}, filepath field: {hit.entity.get('filepath')}")
+# print(search_latency_fmt.format(end_time - start_time))
+
+# res = client.search(
+#     collection_name="test_collection", # Replace with the actual name of your collection
+#     # Replace with your query vector
+#     data=[[0.3580376395471989, -0.6023495712049978, 0.18414012509913835, -0.26286205330961354, 0.9029438446296592]],
+#     limit=5, # Max. number of search results to return
+#     search_params={"metric_type": "IP", "params": {}} # Search parameters
+# )
+
+# Convert the output to a formatted JSON string
+# result = json.dumps(res, indent=4)
+# print(result)
+
+# -----------------------------------------------------------------------------
+# query based on scalar filtering(boolean, int, etc.)
+# print(fmt.format("Start querying with `random > 0.5`"))
+
+# start_time = time.time()
+# result = pgds.query(expr="random > 0.5", output_fields=["random", "embeddings"])
+# end_time = time.time()
+
+# print(f"query result:\n-{result[0]}")
+# print(search_latency_fmt.format(end_time - start_time))
+
+# -----------------------------------------------------------------------------
+# pagination
+# r1 = pgds.query(expr="random > 0.5", limit=4, output_fields=["random"])
+# r2 = pgds.query(expr="random > 0.5", offset=1, limit=3, output_fields=["random"])
+# print(f"query pagination(limit=4):\n\t{r1}")
+# print(f"query pagination(offset=1, limit=3):\n\t{r2}")
+
+
+# -----------------------------------------------------------------------------
+# hybrid search
+# print(fmt.format("Start hybrid searching with `random > 0.5`"))
+
+# start_time = time.time()
+# result = pgds.search(vectors_to_search, "embeddings", search_params, limit=3, expr="random > 0.5", output_fields=["random"])
+# end_time = time.time()
+
+# for hits in result:
+#     for hit in hits:
+#         print(f"hit: {hit}, random field: {hit.entity.get('random')}")
+# print(search_latency_fmt.format(end_time - start_time))
+
+###############################################################################
+# 6. delete entities by PK
+# You can delete entities by their PK values using boolean expressions.
+# ids = insert_result.primary_keys
+
+# expr = f'pk in ["{ids[0]}" , "{ids[1]}"]'
+# print(fmt.format(f"Start deleting with expr `{expr}`"))
+
+# result = pgds.query(expr=expr, output_fields=["random", "embeddings"])
+# print(f"query before delete by expr=`{expr}` -> result: \n-{result[0]}\n-{result[1]}\n")
+
+# pgds.delete(expr)
+
+# result = pgds.query(expr=expr, output_fields=["random", "embeddings"])
+# print(f"query after delete by expr=`{expr}` -> result: {result}\n")
+
+
+###############################################################################
+# 7. drop collection
+# Finally, drop the pgds collection
+# print(fmt.format("Drop collection `pgds`"))
+# utility.drop_collection("pgds")
---
+++ infer.py
@@ -0,0 +1,127 @@
+import os
+from config import cfg
+import argparse
+from datasets import make_dataloader
+from model import make_model
+from processor import do_inference
+from utils.logger import setup_logger
+from torchvision import transforms
+import torchvision
+from PIL import Image
+import cv2 
+import torch 
+import numpy as np
+import streamlit as st
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema, CollectionSchema, DataType,
+    Collection,
+)
+
+from glob import glob 
+
+def list_file_in_folder(path, ext):
+    return glob(path + f"/*.{ext}")
+
+def read_image(img_path):
+    img = torchvision.io.read_image(img_path)
+    
+fmt = "\n=== {:30} ===\n"
+search_latency_fmt = "search latency = {:.4f}s"
+# num_entities, dim = 3000, 8
+dim = 768
+
+if __name__ == "__main__":
+    #-------------------------Load model-------------------------
+    parser = argparse.ArgumentParser(description="ReID Baseline Training")
+    parser.add_argument(
+        "--config_file", default="/Users/JCPHF7G4NK/Documents/projects/thesis/PGDS/configs/market/swin_tiny.yml", help="path to config file", type=str
+    )
+    parser.add_argument("opts", help="Modify config options using the command-line", default=None,
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+    device = "cpu"
+    head_fusion = "max"
+    discard_ratio = 0.9
+    category_index = None
+    w, h = 384, 128
+    transform = transforms.Compose([
+        transforms.Resize((w, h)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+    if args.config_file != "":
+        cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+
+    output_dir = cfg.OUTPUT_DIR
+    if output_dir and not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    logger = setup_logger("transreid", output_dir, if_train=False)
+    logger.info(args)
+
+    if args.config_file != "":
+        logger.info("Loaded configuration file {}".format(args.config_file))
+        with open(args.config_file, 'r') as cf:
+            config_str = "\n" + cf.read()
+            logger.info(config_str)
+    logger.info("Running with config:\n{}".format(cfg))
+
+    # train_loader, train_loader_normal, val_loader, num_query, num_classes, camera_num, view_num = make_dataloader(cfg)
+    model = make_model(cfg, num_class= 77, camera_num=3, view_num = 3, semantic_weight = cfg.MODEL.SEMANTIC_WEIGHT)
+    print(model)
+    model.eval()
+
+    #------------------------End load model-------------------------------------------------
+    
+    #------------------------Generate test embedding vector--------------------------------
+    # embedded_vectors = []
+    # imgs = list_file_in_folder("/Users/JCPHF7G4NK/Documents/projects/thesis/PGDS/DukeMTMC-reID/test", "jpg")
+    # for imagePath in imgs:
+    #     img = Image.open(imagePath).convert('RGB')
+    #     # img = img.resize((w, h))
+    #     # print(img.shape)
+    #     input_tensor = transform(img).unsqueeze(0)
+    #     with torch.no_grad():
+    #         model = model.to(device)
+    #         input_tensor = input_tensor.to(device)
+    #         out = model(input_tensor)
+    #     embedded_vectors.append(out[0])
+    #     # print(out[0].shape)
+    # np.save("/Users/JCPHF7G4NK/Documents/projects/thesis/PGDS/DukeMTMC-reID/testOutput.npy", embedded_vectors)
+    #------------------------Generate test embedding vector--------------------------------
+
+    #------------------------Connect milvus--------------------------------
+    print(fmt.format("start connecting to Milvus"))
+    client = connections.connect("default", host="localhost", port="19530")
+
+    has = utility.has_collection("pgds")
+    print(f"Does collection pgds exist in Milvus: {has}")
+    pgds_col = Collection("pgds")
+    pgds_col.load()
+
+    #------------------------ Connect milvus--------------------------------
+    
+    
+    #------------------------Streamlit search app--------------------------------
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        img = Image.open(uploaded_file).convert('RGB')
+        input_tensor = transform(img).unsqueeze(0)
+        with torch.no_grad():
+            model = model.to(device)
+            input_tensor = input_tensor.to(device)
+            out = model(input_tensor)
+        print(fmt.format("Start searching"))
+        search_params = {
+            "metric_type": "COSINE",
+            "params": {},
+        }
+        vector_to_search = np.array(out[0])
+        result = pgds_col.search(vector_to_search, "embeddings", search_params, limit=20, output_fields=["filepath"])
+        paths = [hit.entity.get('filepath') for hits in result for hit in hits]
+        st.image(paths)
